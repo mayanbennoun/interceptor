37531c9598e20ffaddcf134b20c0c648ebf8d4e8
mayanbennoun <mayanbennoun@gmail.com>
2024-08-13 17:33:43 +0300

    before big change

--- src/rate-limit.service.ts
+++ src/rate-limit.service.ts
@@ -9,40 +9,25 @@ export class RateLimitService {
   constructor(private readonly redisClient: Redis) {}
 
   async isRateLimited(userId: string): Promise<{ isLimited: boolean; remainingRequests: number; retryAfter?: number }> {
-    const record = await this.getRateLimitRecord(userId);
-    const currentRequestTime = Date.now();
-    const windowStartTimestamp = currentRequestTime - this.WINDOW_SIZE_IN_HOURS * 60 * 60 * 1000;
-
-    const requestsWithinWindow = record.filter((entry) => entry > windowStartTimestamp);
-    const totalWindowRequestsCount = requestsWithinWindow.length;
-
-    if (totalWindowRequestsCount >= this.MAX_WINDOW_REQUEST_COUNT) {
-      const oldestRequestTime = requestsWithinWindow[0];
-      const retryAfter = Math.ceil((oldestRequestTime - windowStartTimestamp) / 1000);
-      return { isLimited: true, remainingRequests: 0, retryAfter };
-    } else {
-      record.push(currentRequestTime);
-      await this.saveRateLimitRecord(userId, record);
-      const remainingRequests = this.MAX_WINDOW_REQUEST_COUNT - (totalWindowRequestsCount + 1);
-      return { isLimited: false, remainingRequests };
+    const key = this.getRateLimitKey(userId);
+    const currentCount = await this.redisClient.incr(key);
+    if (currentCount === 1) {
+      // Set expiration for the key when the first request is made
+      await this.redisClient.expire(key, this.WINDOW_SIZE_IN_HOURS * 3600);
     }
+
+    const remainingRequests = this.MAX_WINDOW_REQUEST_COUNT - currentCount;
+    
+    if (remainingRequests < 0) {
+      const ttl = await this.redisClient.ttl(key);
+      return { isLimited: true, remainingRequests: 0, retryAfter: ttl };
+    }
+
+    return { isLimited: false, remainingRequests };
   }
 
-  private async getRateLimitRecord(clientId: string): Promise<number[]> {
-    return new Promise((resolve, reject) => {
-      this.redisClient.get(`rateLimit:${clientId}`, (err, record) => {
-        if (err) reject(err);
-        resolve(record ? JSON.parse(record) : []);
-      });
-    });
-  }
-
-  private async saveRateLimitRecord(clientId: string, record: number[]): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.redisClient.set(`rateLimit:${clientId}`, JSON.stringify(record), (err) => {
-        if (err) reject(err);
-        resolve();
-      });
-    });
+  private getRateLimitKey(userId: string): string {
+    return `rate-limit:${userId}`;
   }
 }
+
