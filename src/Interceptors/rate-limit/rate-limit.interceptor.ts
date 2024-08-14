13a7bbc79b1bdb975c9cfc94aad827aecfc19fba
mayanbennoun <mayanbennoun@gmail.com>
2024-08-14 20:53:15 +0300

    fix:using the http proper types

--- src/Interceptors/rate-limit/rate-limit.interceptor.ts
+++ src/Interceptors/rate-limit/rate-limit.interceptor.ts
@@ -9,6 +9,8 @@ import {
 import { Observable ,throwError} from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { RateLimitService } from './rate-limit.service';
+import { Request, Response } from 'express';
+
 
 
 @Injectable()
@@ -16,9 +18,11 @@ export class RateLimitInterceptor implements NestInterceptor {
     constructor( private readonly rateLimitService: RateLimitService){} 
 
     async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> { 
-        const request = context.switchToHttp().getRequest();
-        const response = context.switchToHttp().getResponse();
-        const userId = request.query.userId;
+        const httpContext = context.switchToHttp();
+        const request: Request = httpContext.getRequest<Request>();
+        const response: Response = httpContext.getResponse<Response>();
+        const userId = request.query.userId as string;
+
         if (!userId) {
             return throwError(() => new HttpException('User ID is required to complete request', HttpStatus.BAD_REQUEST));
           }
